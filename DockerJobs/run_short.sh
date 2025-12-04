e3fcc60afd26d30bcbfafc59c2cf68c8b19cd7f1
ccarlo21 <carlo21cipriani@gmail.com>
2025-12-04 23:39:40 +0100

    Fixed Docker Job short

--- DockerJobs/run_short.sh
+++ DockerJobs/run_short.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+source /opt/conda/etc/profile.d/conda.sh
+
 if [[ $# -ne 1 ]]; then
     echo "Usage: run_short.sh <mandatory_vars.txt>"
     exit 1
@@ -15,6 +17,8 @@ fi
 echo "[INFO] Using config file: $CONF"
 
 # Load conda and activate environment
+RAAVIOLI_SHORT_DIR="/opt/RAAVioli/RAAVioli_short"
+cd "$RAAVIOLI_SHORT_DIR"
 
 RAAVIOLI_SHORT="/opt/RAAVioli/RAAVioli_short/RAAVioli_short.sh"
 
